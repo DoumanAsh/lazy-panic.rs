01ee6b4cdc09cba77972543c6eb7b354c4a1ffe6
Travis CI <travis@testing-worker-linux-docker-64f90f72-3433-linux-4.prod.travis-ci.org>
2016-09-30 16:46:36 +0000

    Auto-update
    Commit: First version(731ff66 2016-09-30)

---
+++ doc/lazy_panic/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"macro":[["format_payload!","Formats ```PanicInfo``` payload into ```String```"],["set_panic_message!","Sets custom printer for panic."]]});
---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["lazy_panic"] = {"doc":"Provides lazy utilities to lazily set custom panic hook.","items":[[14,"format_payload!","lazy_panic","Formats ```PanicInfo``` payload into ```String```",null,null],[14,"set_panic_message!","","Sets custom printer for panic.",null,null]],"paths":[]};
+initSearch(searchIndex);
